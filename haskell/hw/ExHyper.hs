a158f41b4e612e6d7b67f6d06cf14f3a8ee721ea
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2024-10-19 14:29:12 -0300

    ExHyper impl with faulty exp

--- haskell/hw/ExHyper.hs
+++ haskell/hw/ExHyper.hs
@@ -1,28 +1,32 @@
 module ExHyper where
 
-import Prelude hiding ( exp )
+import Prelude hiding (exp)
 
 -- Nat datatype --------------------------------
 
 data Nat = O | S Nat
-     deriving (Eq, Show)
+  deriving (Eq)
 
 instance (Num Nat) where
-    (+) = add
-    (*) = mul
-    abs = id
-    fromInteger 0 = O
-    fromInteger n
-      | n > 0     = S $ fromInteger (n-1)
-      | otherwise = O
-    signum O = O
-    signum n = S O
-    negate n = O
+  (+) = add
+  (*) = mul
+  abs = id
+  fromInteger 0 = O
+  fromInteger n
+    | n > 0 = S $ fromInteger (n - 1)
+    | otherwise = O
+  signum O = O
+  signum n = S O
+  negate n = O
 
 instance (Ord Nat) where
-    O     <= m     = True
-    (S n) <= O     = False
-    (S n) <= (S m) = n <= m
+  O <= m = True
+  (S n) <= O = False
+  (S n) <= (S m) = n <= m
+
+instance (Show Nat) where
+  show 0 = "0"
+  show (S n) = "S" ++ show n
 
 {- explicit definitions of add, mul, exp:
 
@@ -43,17 +47,33 @@ exp n (S m) = mul (exp n m) n
 -- to define each of those functions:
 
 add :: Nat -> Nat -> Nat
-add = hyper undefined
+add = hyper 0
 
 mul :: Nat -> Nat -> Nat
-mul = hyper undefined
+mul = hyper 1
 
 exp :: Nat -> Nat -> Nat
-exp = hyper undefined
+exp = hyper 2
 
 -- hyper n should return the n'th operation in the sequence:
 -- (..?..), add, mul, exp, ...?
 
-hyper :: Integral i => i -> (Nat -> Nat -> Nat)
-hyper = undefined
-
+hyper :: (Integral i) => i -> (Nat -> Nat -> Nat)
+hyper 0 =
+  let add a b =
+        case b of
+          0 -> a
+          (S predB) -> S (add a predB)
+   in add
+hyper 1 =
+  let mul a b =
+        case b of
+          0 -> 0
+          (S predB) -> hyper 0 (mul a predB) b
+   in mul
+hyper 2 =
+  let exp base expoent =
+        case expoent of
+          0 -> 1
+          (S predExpoent) -> hyper 1 base (exp base predExpoent)
+   in exp

d10b46b68e3c209e08a17203478f79a3b89e60a2
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2024-10-29 21:55:10 -0300

    Int.hs: refactor from MkInt to Int, thx joao espada

--- haskell/Int.hs
+++ haskell/Int.hs
@@ -7,42 +7,42 @@ import Prelude hiding (Int)
 import Nat
 
 data Int where
-  MkInt :: (Nat, Nat) -> Int
+  Int :: (Nat, Nat) -> Int
 
 instance Eq Int where
-  MkInt (n, m) == MkInt (n', m') = n + m' == m + n'
+  Int (n, m) == Int (n', m') = n + m' == m + n'
 
 instance Ord Int where
-  MkInt (n, m) <= MkInt (n', m') = (n + m') <= (m + n')
+  Int (n, m) <= Int (n', m') = (n + m') <= (m + n')
 
 instance Enum Int where
   toEnum x
-    | x >= 0 = MkInt (toNat x, 0)
-    | otherwise = MkInt (0, toNat (abs x))
-  fromEnum (MkInt (n, m)) = fromNat n - fromNat m
+    | x >= 0 = Int (toNat x, 0)
+    | otherwise = Int (0, toNat (abs x))
+  fromEnum (Int (n, m)) = fromNat n - fromNat m
 
 instance Num Int where
   (+) = addInt
   (*) = multInt
   (-) = subInt
-  abs (MkInt (n, m)) = if m > n then MkInt (m, n) else MkInt (n, m)
+  abs (Int (n, m)) = if m > n then Int (m, n) else Int (n, m)
   fromInteger x
-    | x >= 0 = MkInt (fromInteger x, 0)
-    | otherwise = MkInt (0, fromInteger (abs x))
+    | x >= 0 = Int (fromInteger x, 0)
+    | otherwise = Int (0, fromInteger (abs x))
 
-  signum (MkInt (n, m))
-    | m > n = MkInt (0, 1)
-    | m == n = MkInt (0, 0)
-    | m <= n = MkInt (1, 0)
+  signum (Int (n, m))
+    | m > n = Int (0, 1)
+    | m == n = Int (0, 0)
+    | m <= n = Int (1, 0)
 
 instance Show Int where
-  show (MkInt (n, m)) = show (fromEnum (MkInt (n, m)))
+  show (Int (n, m)) = show (fromEnum (Int (n, m)))
 
 addInt :: Int -> Int -> Int
-addInt (MkInt (n, m)) (MkInt (n', m')) = MkInt (n + n', m + m')
+addInt (Int (n, m)) (Int (n', m')) = Int (n + n', m + m')
 
 subInt :: Int -> Int -> Int
-subInt (MkInt (n, m)) (MkInt (n', m')) = MkInt (n + m', m + n')
+subInt (Int (n, m)) (Int (n', m')) = Int (n + m', m + n')
 
 multInt :: Int -> Int -> Int
-multInt (MkInt (n, m)) (MkInt (n', m')) = MkInt (n * n' + m * m', n * m' + m * n')
+multInt (Int (n, m)) (Int (n', m')) = Int (n * n' + m * m', n * m' + m * n')

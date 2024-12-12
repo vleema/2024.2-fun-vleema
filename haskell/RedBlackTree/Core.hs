10a8a84ab8de3284aa56cb20e000447abf4fb655
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2024-12-12 08:41:44 -0300

    RedBlackTree: rotations

--- haskell/RedBlackTree/Core.hs
+++ haskell/RedBlackTree/Core.hs
@@ -2,9 +2,7 @@ module RedBlackTree.Core where
 
 import Direction (Direction (..), Path)
 
-data Color where
-  Red :: Color
-  Black :: Color
+data Color = Red | Black
 
 data RedBlackTree a
   = Node
@@ -19,5 +17,13 @@ search :: (Ord a) => a -> RedBlackTree a -> Maybe Path
 search _ Nil = Nothing
 search a (Node value leftTree rightTree _)
   | a == value = Just []
-  | a > value = fmap (R :) (search value rightTree)
-  | a < value = fmap (L :) (search value leftTree)
+  | a > value = (R :) <$> search value rightTree
+  | a < value = (L :) <$> search value leftTree
+
+rotateLeft :: RedBlackTree a -> RedBlackTree a
+rotateLeft (Node a leftTree (Node b bLeftTree bRightTree bColor) color) =
+  Node b (Node a leftTree bLeftTree color) bRightTree bColor
+
+rotateRight :: RedBlackTree a -> RedBlackTree a
+rotateRight (Node a (Node b bLeftTree bRightTree bColor) rightTree color) =
+  Node b bLeftTree (Node a bRightTree rightTree color) bColor
